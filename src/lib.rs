965a76876afe2cbac03e74185631fa8c6d970fdd
kyledunne <kyledunne@berkeley.edu>
2018-01-08 18:59:33 -0500

    Add tests using dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
+extern crate glutin;
+extern crate gl;
+
 #[cfg(test)]
 mod tests {
+    extern crate glutin;
+    extern crate gl;
+
     #[test]
     fn it_works() {
+        glutin::EventsLoop::new();
+        println!("rug2d: test version sucessfully executed (from tests::it_works()).");
         assert_eq!(2 + 2, 4);
     }
 }
 
 pub fn info() {
-    println!("rug2d: test version.")
+    glutin::EventsLoop::new();
+    println!("rug2d: test version sucessfully executed.")
 }
