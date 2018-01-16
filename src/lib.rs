ae0a0ba0668ed99de6dea1e69bc101ad5f08a732
kyledunne <kyledunne@berkeley.edu>
2018-01-16 03:44:49 -0800

    Add create, render, check close events functionality

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use sdl2::keyboard::Keycode;
 use sdl2::video::GLProfile;
 
 static DEFAULT_WINDOW_SIZE: [u32; 2] = [1024, 768];
+static mut GRAPHICS_INITIALIZED: bool = false;
 
 #[cfg(test)]
 mod tests {
@@ -22,41 +23,69 @@ pub fn info() {
 }
 
 pub fn test_fn() {
-    let sdl_context = sdl2::init().unwrap();
+    let mut window = init_window("Sup", 1000, 500);
+    'running: loop {
+        window.render();
+        if window.check_events() {
+            break 'running;
+        }
+        //TODO set up a sync() method that considers the nanos since the last call to sync
+        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
+pub fn init_window(title: &str, w: u32, h: u32) -> Rug2dWindow {
+    let sdl_context: sdl2::Sdl = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let gl_attr = video_subsystem.gl_attr();
     gl_attr.set_context_profile(GLProfile::Core);
     gl_attr.set_context_version(3, 3);
 
-    let window = video_subsystem.window("Window", 800, 600)
+    let window: sdl2::video::Window = video_subsystem.window(title, w, h)
         .opengl()
         .build()
         .unwrap();
 
-    let ctx = window.gl_create_context().unwrap();
+    window.gl_create_context().unwrap();
     gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
 
     debug_assert_eq!(gl_attr.context_profile(), GLProfile::Core);
     debug_assert_eq!(gl_attr.context_version(), (3, 3));
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let event_pump: sdl2::EventPump = sdl_context.event_pump().unwrap();
 
-    'running: loop {
+    //'running: loop {
+    //::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+
+    Rug2dWindow {
+        window,
+        event_pump,
+    }
+}
+
+pub struct Rug2dWindow {
+    window: sdl2::video::Window,
+    event_pump: sdl2::EventPump,
+} impl Rug2dWindow {
+    pub fn render(&self) {
         unsafe {
+            //TODO figure out why this color isn't appearing
             gl::ClearColor(0.6, 0.0, 0.8, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
         }
-
-        window.gl_swap_window();
-        for event in event_pump.poll_iter() {
+        self.window.gl_swap_window();
+    }
+    pub fn check_events(&mut self) -> bool {
+        for event in self.event_pump.poll_iter() {
             match event {
                 Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
+                    println!("wow!");
+                    return true;
                 },
                 _ => {}
             }
         }
-        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+        false
     }
 }
